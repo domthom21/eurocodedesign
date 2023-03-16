87e7212c924380cb6090cfff4a3d52a8a69fb475
nc-hsu <nicholas.clemett@hsu-hh.de>
2023-03-16 22:16:14 +0100

    new steel section classes

--- eurocodedesign/geometry/steelsection/steelsections.py
+++ eurocodedesign/geometry/steelsection/steelsections.py
@@ -9,13 +9,64 @@ from dataclasses import dataclass, field
 @dataclass(frozen=True)
 class SteelSection(BasicSection):
     # add functionality if required later
-    
+
     # ??? possible funcitonality could include calculation of axial, shear, and
     # ??? bending moment strengths -- with a steel material class as input
     pass
 
+
 @dataclass(frozen=True)
-class RolledISection(SteelSection):
+class RolledSection(SteelSection):
+    pass
+
+
+@dataclass(frozen=True)
+class WeldedSection(SteelSection):
+    pass
+
+
+@dataclass(frozen=True)
+class HollowSection(SteelSection):
+    pass
+
+
+@dataclass(frozen=True)
+class FullSection(SteelSection):
+    pass
+
+
+@dataclass(frozen=True)
+class ISection(SteelSection):
+    pass
+
+
+@dataclass(frozen=True)
+class SquareSection(SteelSection):
+    pass
+
+
+@dataclass(frozen=True)
+class RectSection(SteelSection):
+    pass
+
+
+@dataclass(frozen=True)
+class CircSection(SteelSection):
+    pass
+
+
+@dataclass(frozen=True)
+class HotFormed(SteelSection):
+    pass
+
+
+@dataclass(frozen=True)
+class ColdFormed(SteelSection):
+    pass
+
+
+@dataclass(frozen=True)
+class RolledISection(RolledSection, ISection):
     height: float
     flange_width: float
     web_thickeness: float
@@ -41,4 +92,36 @@ class RolledISection(SteelSection):
 
     def print_properties(self) -> None:
         # TODO: implement a nice way of printing the properties
-        raise NotImplementedError("IProfile.print_propteries() is not yet implemented")
+        raise NotImplementedError(
+            "RolledISection.print_propteries() is not yet implemented"
+        )
+
+
+@dataclass(frozen=True)
+class CircHollowSection(HollowSection, CircSection):
+    diameter: float
+    wall_thickness: float
+    weight: float
+    perimeter: float
+    area: float
+    shear_area: float
+    second_moment_of_area: float
+    radius_of_gyration: float
+    elastic_section_modulus: float
+    plastic_section_modulus: float
+    torsion_constant: float
+    torsion_modulus: float
+
+    def print_properties(self) -> None:
+        # TODO: implement a nice way of printing the properties
+        raise NotImplementedError(
+            "CircHollowSection.print_propteries() is not yet implemented"
+        )
+
+
+class ColdCircHollowSection(ColdFormed, CircHollowSection):
+    pass
+    
+
+class HotCircHollowSection(HotFormed, CircHollowSection):
+    pass
