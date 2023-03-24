45e6b3a14f1db054b19e86c173332be0ee27e578
nc-hsu <nicholas.clemett@hsu-hh.de>
2023-03-24 15:29:35 +0100

    Adds tests for class properties and get method

--- eurocodedesign/materials/structuralsteel.py
+++ eurocodedesign/materials/structuralsteel.py
@@ -13,8 +13,18 @@ following standards:
     
 All material strengths are given in MPa (N/mm2) unless otherwise indicated
 
-# todo: add example of how to get properties for different thickness 
-# todo: materials
+Example Usage:
+
+    import eurocodedesign.materials.structuralsteel as ss
+    steel_material = ss.get("S235", True) # S235 steel material with thickness <= 40 mm
+    steel_material = ss.get("S355", False) # S355 steel material with thickness >= 40 mm
+    
+    yield_stress = steel_material.fy
+    ultimate_stress = steel_material.fu
+    
+    elastic_modulus = steel_material.E 
+    shear_modulus = steel_material.G
+    thermal_coefficient = steel_material.alpha  
 """
 
 from dataclasses import dataclass, field
@@ -22,82 +32,95 @@ from dataclasses import dataclass, field
 
 @dataclass(frozen=True)
 class BasicStructuralSteel:
-    thickness_less_than_40mm: bool = True
-    elastic_modulus: float = field(default=210_000, kw_only=True)
-    shear_modulus: float = field(default=81_000, kw_only=True)
+    _thickness_less_than_equal_40mm: bool = True
+    _elastic_modulus: float = field(default=210_000, kw_only=True)
+    _shear_modulus: float = field(default=81_000, kw_only=True)
+    _thermal_coefficient: float = field(default=1.2e-7, kw_only=True)  # 1/K
     poissons_ratio: float = field(default=0.3, kw_only=True)
-    thermal_coefficient: float = field(default=1.2e-7, kw_only=True)  # 1/K
 
     @property
     def fy(self):
-        if self.thickness_less_than_40mm:
-            return self.fy_thin
+        if self._thickness_less_than_equal_40mm:
+            return self._fy_thin
         else:
-            return self.fy_thick
+            return self._fy_thick
 
     @property
     def fu(self):
-        if not self.thickness_less_than_40mm:
-            return self.fu_thin
+        if not self._thickness_less_than_equal_40mm:
+            return self._fu_thin
         else:
-            return self.fu_thick
+            return self._fu_thick
+
+    @property
+    def E(self):
+        return self._elastic_modulus
+
+    @property
+    def G(self):
+        return self._shear_modulus
+
+    @property
+    def alpha(self):
+        return self._thermal_coefficient
 
 
 @dataclass(frozen=True)
 class S235(BasicStructuralSteel):
     name: float = field(default="S235", kw_only=True)
-    fy_thin: float = field(default=235, kw_only=True)
-    fu_thin: float = field(default=360, kw_only=True)
-    fy_thick: float = field(default=215, kw_only=True)
-    fu_thick: float = field(default=360, kw_only=True)
+    _fy_thin: float = field(default=235, kw_only=True)
+    _fu_thin: float = field(default=360, kw_only=True)
+    _fy_thick: float = field(default=215, kw_only=True)
+    _fu_thick: float = field(default=360, kw_only=True)
     norm: str = field(default="EN 10025-2", kw_only=True)
 
 
 @dataclass(frozen=True)
 class S275(BasicStructuralSteel):
     name: float = field(default="S275", kw_only=True)
-    fy_thin: float = field(default=275, kw_only=True)
-    fu_thin: float = field(default=430, kw_only=True)
-    fy_thick: float = field(default=255, kw_only=True)
-    fu_thick: float = field(default=410, kw_only=True)
+    _fy_thin: float = field(default=275, kw_only=True)
+    _fu_thin: float = field(default=430, kw_only=True)
+    _fy_thick: float = field(default=255, kw_only=True)
+    _fu_thick: float = field(default=410, kw_only=True)
     norm: str = field(default="EN 10025-2", kw_only=True)
 
 
 @dataclass(frozen=True)
 class S355(BasicStructuralSteel):
     name: float = field(default="S355", kw_only=True)
-    fy_thin: float = field(default=355, kw_only=True)
-    fu_thin: float = field(default=490, kw_only=True)
-    fy_thick: float = field(default=335, kw_only=True)
-    fu_thick: float = field(default=470, kw_only=True)
+    _fy_thin: float = field(default=355, kw_only=True)
+    _fu_thin: float = field(default=490, kw_only=True)
+    _fy_thick: float = field(default=335, kw_only=True)
+    _fu_thick: float = field(default=470, kw_only=True)
     norm: str = field(default="EN 10025-2", kw_only=True)
 
 
 @dataclass(frozen=True)
 class S450(BasicStructuralSteel):
     name: float = field(default="S450", kw_only=True)
-    fy_thin: float = field(default=440, kw_only=True)
-    fu_thin: float = field(default=550, kw_only=True)
-    fy_thick: float = field(default=410, kw_only=True)
-    fu_thick: float = field(default=550, kw_only=True)
+    _fy_thin: float = field(default=440, kw_only=True)
+    _fu_thin: float = field(default=550, kw_only=True)
+    _fy_thick: float = field(default=410, kw_only=True)
+    _fu_thick: float = field(default=550, kw_only=True)
     norm: str = field(default="EN 10025-2", kw_only=True)
 
 
 """ 
-constants
+Constants
 """
 
 STEEL_TYPES = {"S235": S235, "S275": S275, "S355": S355, "S450": S450}
 
-
 """ 
-definitions
+Definitions
 """
 
-def get(steel_type: str) -> BasicStructuralSteel:
-    pass
+
+def get(
+    steel_type: str, thickness_less_than_equal_40mm: bool = True
+) -> BasicStructuralSteel:
+    return STEEL_TYPES[steel_type](thickness_less_than_equal_40mm)
 
 
 if __name__ == "__main__":
-    steel = S235(24)
-    print(steel.thickness)
+    steel = get("s121")
--- tests/test_structuralsteel.py
+++ tests/test_structuralsteel.py
@@ -1,18 +1,18 @@
 """ Tests for the constructionsteel module
 """
 
-from pytest import fixture
-from eurocodedesign.materials.structuralsteel import S235
+from pytest import fixture, raises
+import eurocodedesign.materials.structuralsteel as ss
 
 
 @fixture
 def S235_thin_material():
-    return S235()
+    return ss.S235()
 
 
 @fixture
 def S235_thick_material():
-    return S235(False)
+    return ss.S235(False)
 
 
 def test_fy_getter_with_thin_material(S235_thin_material):
@@ -29,5 +29,32 @@ def test_fy_getter_with_thick_material(S235_thick_material):
 
 def test_fu_getter_with_thick_material(S235_thick_material):
     assert S235_thick_material.fu == 360
-    
-    
+
+
+def test_get_for_structural_steel_types_with_steel_in_library_t_less_equal_40(
+    S235_thin_material,
+):
+    assert ss.get("S235") == S235_thin_material
+
+
+def test_get_for_structural_steel_types_with_steel_in_library_t_greater_40(
+    S235_thick_material,
+):
+    assert ss.get("S235", False) == S235_thick_material
+
+
+def test_get_for_structural_steel_types_with_steel_not_in_library():
+    with raises(KeyError):
+        ss.get("211", True)
+
+
+def test_property_E(S235_thick_material):
+    assert S235_thick_material.E == 210_000
+
+
+def test_property_G(S235_thick_material):
+    assert S235_thick_material.G == 81_000
+
+
+def test_property_G(S235_thick_material):
+    assert S235_thick_material.alpha == 1.2e-7
