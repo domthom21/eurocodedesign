c4682139f2c82b8bda558ba92cab96d431683e93
Dominik Thomas <dominik.thomas@hsu-hh.de>
2023-08-29 16:13:15 +0200

    Resolve comments
    Include feedback and improve docstrings

--- eurocodedesign/standard/ec3/platebuckling/reduced_stress_method.py
+++ eurocodedesign/standard/ec3/platebuckling/reduced_stress_method.py
@@ -1,19 +1,21 @@
-from enum import Enum, auto
 from typing import Literal, NoReturn
 
 import numpy as np
 
-from eurocodedesign.core.typing import FloatSequence, FloatTriple, Eta
+from eurocodedesign.core.typing import FloatTriple, Eta
 from eurocodedesign.standard.ec3 import gamma_M1
+from eurocodedesign.standard.ec3.platebuckling import PlateSupport
+from eurocodedesign.standard.ec3.platebuckling.effective_width_method import \
+    calc_rho
 from eurocodedesign.stepper import inject_stepper, Stepper
 from eurocodedesign.units import Pascal
 
 
 @inject_stepper
 def is_permitted(rho: float, alpha_ultk: float, stepper: Stepper) -> bool:
-    """Check prerequisites for the reduced stress method of plate buckling
+    r"""Check prerequisites for the reduced stress method of plate buckling
 
-    Calculation according to EN 1993-1-5:2019-10 §2.4, §10 (1) and §10 (2)
+    Calculation according to EN 1993-1-5:2019-10 §2.4, §10 (1) and §10 (2).
 
     The reduced stress method is allowed, if the cross-section can be
     classified as class 3 cross-section according to §10 (2).
@@ -22,8 +24,8 @@ def is_permitted(rho: float, alpha_ultk: float, stepper: Stepper) -> bool:
     plates.
 
     Args:
-        rho: reduction factor ``rho`` from §10 (5), see calc_rho
-        alpha_ultk: minimum load amplifier ``\\alpha_{ult,k}`` of the design
+        rho: reduction factor :math:`\rho` from §10 (5), see calc_rho
+        alpha_ultk: minimum load amplifier :math:`\alpha_{ult,k}` of the design
          loads, see calc_alpha_ultk
         stepper: Stepper object to which calculcation steps are added
 
@@ -41,9 +43,9 @@ def is_permitted(rho: float, alpha_ultk: float, stepper: Stepper) -> bool:
     return valid
 
 
-def calc_bar_lambda_p(alpha_ultk: FloatSequence,
-                      alpha_cr: FloatSequence) -> FloatSequence:
-    """Calculate the plate slenderness ``\\bar{\\lambda}_p``
+def calc_bar_lambda_p(alpha_ultk: float,
+                      alpha_cr: float) -> float:
+    r"""Calculate the plate slenderness :math:`\bar{\lambda}_p`
     of the plate buckling field according to EN 1993-1-5:2019-10 §10 (3)
 
    Args:
@@ -54,10 +56,10 @@ def calc_bar_lambda_p(alpha_ultk: FloatSequence,
         (minimum force amplifier), see calc_alpha_cr
 
     Returns:
-        FloatSequence: The plate slenderness ``\\bar{\\lambda}_p``.
+        FloatSequence: The plate slenderness :math:`\bar{\lambda}_p``.
 
     """
-    bar_lambda_p: FloatSequence = np.sqrt(np.divide(alpha_ultk, alpha_cr))
+    bar_lambda_p: float = np.sqrt(alpha_ultk / alpha_cr)
     return bar_lambda_p
 
 
@@ -65,20 +67,20 @@ def calc_alpha_ultk(f_y: Pascal,
                     sigma_x_Ed: Pascal,
                     sigma_z_Ed: Pascal,
                     tau_Ed: Pascal) -> float:
-    """Calculate ``\\alpha_{ult,k}`` for plate buckling
+    r"""Calculate :math:`\alpha_{ult,k}` for plate buckling
 
-    ``\\alpha_{ult,k}`` is the minimum load amplifier of the design loads to
+    :math:`\alpha_{ult,k}` is the minimum load amplifier of the design loads to
      reach the characteristic resistance of the most critical cross-section
     Calculations according to EN 1993-1-5:2019 §10 (4).
 
     Args:
-        f_y: yield strength ``f_y``
-        sigma_x_Ed: design value ``\\sigma_{x,Ed}`` of the longitudinal stress
-        sigma_z_Ed: design value ``\\sigma_{z,Ed}`` of the transverse stress
-        tau_Ed: design value ``\\tau_{Ed}`` of the shear stress
+        f_y: yield strength :math:`f_y`
+        sigma_x_Ed: design value :math:`\sigma_{x,Ed}` of the longitudinal stress
+        sigma_z_Ed: design value :math:`\sigma_{z,Ed}` of the transverse stress
+        tau_Ed: design value :math:`\tau_{Ed}` of the shear stress
 
     Returns:
-        FloatSequence: ``\\alpha_{ult,k}``: minimum load amplifier of the
+        float: :math:`\alpha_{ult,k}`: minimum load amplifier of the
          design loads
     """
     psi_x_Ed: float = sigma_x_Ed / f_y
@@ -95,25 +97,25 @@ def calc_alpha_ultk(f_y: Pascal,
 
 def calc_alpha_cr(alpha_crx: float, alpha_crz: float, alpha_crtau: float,
                   psi_x: float, psi_z: float) -> float:
-    """Calculates ``\\alpha_{cr}`` from ``\\sigma_{x,Ed}, \\sigma_{z,Ed},
-     \\tau_{Ed}``
+    r"""Calculates :math:`\alpha_{cr}` from
+    :math:`\sigma_{x,Ed}, \sigma_{z,Ed}, \tau_{Ed}`
 
-    Necessary if no single ``\\alpha_cr`` for the complete stress field, but
-    ``\\alpha_{cr,x}, \\alpha_{cr,z}, \\alpha_{cr,\\tau}`` are given.
+    Necessary if no single :math:`\alpha_cr`` for the complete stress field,
+     but :math:`\alpha_{cr,x}, \alpha_{cr,z}, \alpha_{cr,\tau}` are given.
 
     Calculations according to EN 1993-1-5:2019-10 §10 (6)
 
     Args:
-        alpha_crx: ``\\alpha_{cr,x} = \\frac{\\sigma_{cr,x}}{\\sigma_{x,Ed}}``
-        alpha_crz: ``\\alpha_{cr,z} = \\frac{\\sigma_{cr,z}}{\\sigma_{z,Ed}}``
-        alpha_crtau: ``\\alpha_{cr,\\tau} = \\frac{\\tau_{cr}}{\\tau_{Ed}}``
-        psi_x: Ratio ```\\psi_x`` of maximum vs minimum applied stress in
+        alpha_crx: :math:`\alpha_{cr,x}=\frac{\sigma_{cr,x}}{\sigma_{x,Ed}}`
+        alpha_crz: :math:`\alpha_{cr,z}=\frac{\sigma_{cr,z}}{\sigma_{z,Ed}}`
+        alpha_crtau: :math:`\alpha_{cr,\tau}=\frac{\\tau_{cr}}{\tau_{Ed}}`
+        psi_x: Ratio :math:`\psi_x` of maximum vs minimum applied stress in
         x-direction
-        psi_z: Ratio ```\\psi_z`` of maximum vs minimum applied stress in
+        psi_z: Ratio :math:`\psi_z` of maximum vs minimum applied stress in
         z-direction
 
     Returns:
-        float: ` critical load factor `\\alpha_{cr}}`` for plate buckling
+        float: critical load factor :math:`\alpha_{cr}` for plate buckling
     """
     inv_alpha_cr: float = ((1 + psi_x) / (4 * alpha_crx)
                            + (1 + psi_z) / (4 * alpha_crz)
@@ -126,54 +128,8 @@ def calc_alpha_cr(alpha_crx: float, alpha_crz: float, alpha_crtau: float,
     return alpha_cr
 
 
-class PlateSupport(Enum):
-    """ Class for differentation between one-side and two-side supported
-    buckling fields according to EN 1993-1-5 §4.4
-    """
-    ONE_SIDE = auto()
-    TWO_SIDE = auto()
-
-
-# TODO move to reduced_width_method module in platebuckling
-def _calc_rho(support: PlateSupport,
-              bar_lambda_p: float,
-              psi: float = 1.0) -> float:
-    """Calculates the reduction factor ``\\rho``
-
-    Calculation for two side supported and one side supported buckling plates
-     without stiffeners.
-
-    Calculation according to EN 1993-1-5:2019-10 §4.4.
-
-    Args:
-        support: PlateSupport.ONE_SIDE or PlateSupport.TWO_SIDE
-        bar_lambda_p: relative slenderness ``\\bar{\\lambda}_p`` for
-        plate buckling, see calc_bar_lambda_p
-        psi: stress ratio ``\\psi``
-
-    Returns:
-        float: ``\\rho`` reduction factor
-    """
-    if not isinstance(support, PlateSupport):
-        raise TypeError('support must be an instance of Enum PlateSupport')
-
-    if support == PlateSupport.ONE_SIDE:
-        min_bar_lambda_p = 0.748
-    if support == PlateSupport.TWO_SIDE:
-        min_bar_lambda_p = 0.5 + np.sqrt(0.085 - 0.055 * psi)
-    if bar_lambda_p <= min_bar_lambda_p:
-        rho = 1.0
-    else:  # bar_lambda_p > min_bar_lambda_p
-        if support == PlateSupport.ONE_SIDE:
-            rho = np.divide(bar_lambda_p - 0.188, bar_lambda_p ** 2)
-        elif support == PlateSupport.TWO_SIDE:
-            rho = np.divide(bar_lambda_p - 0.055*(3 + psi), bar_lambda_p ** 2)
-        rho = 1.0 if rho > 1.0 else rho
-    return rho
-
-
 def _calc_chi_w() -> NoReturn:
-    """Calculate the reduction factor``\\Chi_w`` for shear buckling
+    r"""Calculate the reduction factor :math:`\chi_w` for shear buckling
 
     Calculation according to EN 1993-1-5:2019-10 §5.3(1).
     """
@@ -188,30 +144,29 @@ def calc_rho(method: ReductionMethod,
              bar_lambda_p: float,
              psi_x: float,
              psi_z: float) -> FloatTriple:
-    """Calculate reduction factors for the reduced stress method
+    r"""Calculate reduction factors for the reduced stress method
 
-    Returns reduction factor ``\\rho_x`` for plate buckling in x-direction
-        rho_z: reduction factor ``\\rho_z`` for plate buckling in z-direction
-        chi_w: reduction factor ``\\Chi_w`` for shear buckling
+    Returns the reduction factors :math:`\rho_x, \rho_z, \chi_w` for
+     plate buckling in x-direction, z-direction and shear buckling respectively
 
-    ``\\Chi_w`` is currently not supported and set to 1.0
+    :math:`\chi_w` is currently not supported and set to 1.0
 
     Calculation according to EN 1993-1-5:2019-10 §10 (5)
 
     Args:
         method: 'smallest' or 'interpolated'
         support: PlateSupport.ONE_SIDE or PlateSupport.TWO_SIDE
-        bar_lambda_p: plate slenderness ``\\bar{\\lambda}_p``
-        psi_x: stress ratio ``\\psi_x`` in x-direction
-        psi_z: stress ratio ``\\psi_z`` in z-direction
+        bar_lambda_p: plate slenderness :math:`\bar{\lambda}_p``
+        psi_x: stress ratio :math:`\psi_x` in x-direction
+        psi_z: stress ratio :math:`\psi_z` in z-direction
 
     Returns:
-        FloatTriple:  ``\\rho_x, \\rho_z, \\Chi_w``
+        FloatTriple:  :math:`\rho_x, \rho_z, \chi_w`
 
     """
-    rho_x: float = _calc_rho(support, bar_lambda_p, psi_x)
+    rho_x: float = calc_rho(support, bar_lambda_p, psi_x)
     # Note for rho_z, §6 is neglected, instead 4.5.4(1) is used
-    rho_z: float = _calc_rho(support, bar_lambda_p, psi_z)
+    rho_z: float = calc_rho(support, bar_lambda_p, psi_z)
     # chi_w: float = _calc_chi_w() # Currently not supported
     chi_w: float = 1.0
 
@@ -230,22 +185,26 @@ def calc_eta(f_y: Pascal,
              rho_x: float,
              rho_z: float,
              chi_w: float) -> Eta:
-    """Calculates the load factor ``\\eta``
+    r"""Calculates the load factor :math:`\eta`
 
     Calculates the load factor for plate buckling with the reduced stress
     method according to EN 1995-1-5:2019-10 §10 (5)
 
     Args:
-        f_y: yield strength ``f_y``
-        sigma_x_Ed: design value ``\\sigma_{x,Ed}`` of the longitudinal stress
-        sigma_z_Ed: design value ``\\sigma_{z,Ed}`` of the transverse stress
-        tau_Ed: design value ``\\tau_{Ed}``of the shear stress
-        rho_x: reduction factor ``\\rho_x`` for plate buckling in x-direction
-        rho_z: reduction factor ``\\rho_z`` for plate buckling in y-direction
-        chi_w: reduction factor ``\\Chi_w`` for shear buckling
+        f_y: yield strength :math:`f_y`
+        sigma_x_Ed: design value :math:`\sigma_{x,Ed}` of the
+        longitudinal stress
+        sigma_z_Ed: design value :math:`\sigma_{z,Ed}` of the
+        transverse stress
+        tau_Ed: design value :math:`\tau_{Ed}` of the shear stress
+        rho_x: reduction factor :math:`\rho_x` for plate buckling in
+         x-direction
+        rho_z: reduction factor :math:`\rho_z` for plate buckling in
+         y-direction
+        chi_w: reduction factor :math:`\chi_w` for shear buckling
 
     Returns:
-        Eta: load factor ``\\eta``
+        Eta: load factor :math:`\eta`
 
     """
     # Interaction is already contained in this equation,
